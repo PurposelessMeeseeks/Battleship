67929013c96ebc9b35c1d19383d3610471f65c94
Ivan Čulina <72503859+ivculina@users.noreply.github.com>
2021-03-24 21:23:30 +0100

    Add Model and UnitTests projects

---
+++ UnitTests/TestSquare.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Vsite.Oom.Battleship.Model.UnitTests
+{
+    [TestClass]
+    public class TestSquare
+    {
+        [TestMethod]
+        public void SquareConstructorCreateASquareAtGivenRowAndColumn()
+        {
+            Square s = new Square(5, 4);
+            Assert.AreEqual(5, s.Row); 
+            Assert.AreEqual(4, s.Column);
+        }
+    }
+}
