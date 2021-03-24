537c87d906050f3ea7e6d5e330d2d20c429a1c37
Jasmin Husić <jhsc1988@gmail.com>
2021-03-24 21:23:12 +0100

    Add Model and UnitTests projects, add Square structure.

---
+++ UnitTests/TestSquare.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Vsite.Oom.Battleship.Model.UnitTests
+{
+    [TestClass]
+    public class TestSquare
+    {
+        [TestMethod]
+        public void SquareConstructorCreatesASquareAtGivenRowAndColumn()
+        {
+            Vsite.Oom.Battleship.Model.
+            Square s = new Square(5, 4);
+            Assert.AreEqual(5, s.Row);
+            Assert.AreEqual(5, s.Column);
+
+        }
+    }
+}
