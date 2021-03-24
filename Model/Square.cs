9554d552c8089a6f9a092650acc6eb1dcfe13f17
Topesa <pesa710@gmail.com>
2021-03-24 21:22:30 +0100

    Add Model and UnitTests projects, add Square struct

---
+++ Model/Square.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Vsite.Oom.Battleship.Model
+{
+    public struct Square
+    {
+        public Square(int Row, int Column)
+        {
+            row = Row;
+            column = Column;
+        }
+
+        public readonly int row;
+        public readonly int column;
+    }
+}
