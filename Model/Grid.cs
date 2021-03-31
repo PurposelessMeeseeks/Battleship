ea17fea1de544af41691b9320f1bc827329bbd9f
Ivan Mihin <56129012+Ivan-Mihin@users.noreply.github.com>
2021-03-31 21:09:42 +0200

    Implement GetAvailablePlacements for horizontal sequences

--- Model/Grid.cs
+++ Model/Grid.cs
@@ -13,23 +13,54 @@ namespace Vsite.Oom.Battleship.Model
             this.Rows = rows;
             this.Columns = columns;
 
-            squares = new Square?[rows, columns];
+            Squares = new Square?[rows, columns];
             for (int r = 0; r < rows; ++r)
             {
                 for (int c = 0; c < columns; ++c)
                 {
-                    squares[r, c] = new Square(r, c);
+                    Squares[r, c] = new Square(r, c);
                 }
             }
         }
 
         public IEnumerable<IEnumerable<Square>> GetAvailablePlacements(int length)
         {
-            return new List<List<Square>>();
+            List<List<Square>> result = GetHorizontalPlacements(length);
+            // add GetVerticalPlacements(length)
+
+            return result;
+        }
+
+        private List<List<Square>> GetHorizontalPlacements(int length)
+        {
+            var result = new List<List<Square>>();
+
+            for (int r = 0; r < Rows; ++r)
+            {
+                LimitedQueue<Square> gathered = new LimitedQueue<Square>(length);
+                for (int c = 0; c < Columns; ++c)
+                {
+                    if (Squares[r, c] != null)
+                    {
+                        gathered.Enqueue(Squares[r, c].Value);
+                    }
+                    else
+                    {
+                        gathered.Clear();
+                    }
+
+                    if (gathered.Count == length)
+                    {
+                        result.Add(new List<Square>(gathered.ToArray<Square>()));
+                    }
+                }
+            }
+
+            return result;
         }
 
         private int Rows;
         private int Columns;
-        private Square?[,] squares;
+        private Square?[,] Squares;
     }
 }
