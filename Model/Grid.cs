cd53d51421bb57498a280266f89684ae9653823b
Ante Žunić <anzunic@vsite.hr>
2021-03-31 21:09:41 +0200

    Implements GetAvailablePlacements for horizonal sequences

--- Model/Grid.cs
+++ Model/Grid.cs
@@ -24,8 +24,37 @@ namespace Vsite.Oom.Battleship.Model
         }
         public IEnumerable<IEnumerable<Square>> GetAvailablePlacements(int length)
         {
-            return new List<List<Square>>();
+            List<List<Square>> result = GetHorizontalPlacements(length);
+            //Add vertical placements
+            //result.Add()
+
+
+            return result;
         }
+
+        private List<List<Square>> GetHorizontalPlacements(int length)
+        {
+            var result = new List<List<Square>>();
+            for (int r = 0; r < rows; ++r)
+            {
+                LimitedQueue<Square> gathered = new LimitedQueue<Square>(length);
+                for (int c = 0; c < columns; ++c)
+                {
+                    if (squares[r, c] != null)
+                        gathered.Enqueue(squares[r, c].Value);
+                    else
+                        gathered.Clear();
+                    if (gathered.Count == length)
+                    {
+                        result.Add(new List<Square>(gathered.ToArray<Square>()));
+                    }
+                }
+            }
+            return result;
+        }
+
+
+
         private int rows;
         private int columns;
 
