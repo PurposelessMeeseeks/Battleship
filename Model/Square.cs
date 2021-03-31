35772e569c92d839fa1e06d6821c66a2bcda117f
Senad Elezović <senad.elezovic@luvis.hr>
2021-03-31 17:39:53 +0200

    Battleship solution first commit

---
+++ Model/Square.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Vsite.Oom.Battleship.Model
+{
+    public struct Square
+    {
+        public Square(int row, int column)
+        {
+            _row = row;
+            _column = column;
+        }
+
+        public int _row;
+        public int _column;
+
+    }
+}
+
+
---
+++ UnitTest/TestSquare.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace Vsite.Oom.Battleship.Model.UnitTest
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestingSquareConstructorsRowAndColumn()
+        {
+            Square testingSquare = new Square(5, 4);
+            Assert.AreEqual(5, testingSquare._row);
+            Assert.AreEqual(4, testingSquare._column);
+
+        }
+    }
+}
