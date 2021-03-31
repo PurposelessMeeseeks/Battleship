76ddd0f39fff0664cbf56e9afce2ca5d47f70671
Ivan Mihin <56129012+Ivan-Mihin@users.noreply.github.com>
2021-03-31 20:26:27 +0200

    Create Grid.cs

---
+++ Model/Grid.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Vsite.Oom.Battleship.Model
+{
+    public class Grid
+    {
+        public Grid(int rows, int columns)
+        {
+            this.Rows = rows;
+            this.Columns = columns;
+
+            squares = new Square?[rows, columns];
+            for (int r = 0; r < rows; ++r)
+            {
+                for (int c = 0; c < columns; ++c)
+                {
+                    squares[r, c] = new Square(r, c);
+                }
+            }
+        }
+
+        public IEnumerable<IEnumerable<Square>> GetAvailablePlacements(int length)
+        {
+            return new List<List<Square>>();
+        }
+
+        private int Rows;
+        private int Columns;
+        private Square?[,] squares;
+    }
+}
