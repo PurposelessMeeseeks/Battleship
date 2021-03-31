7d454dbbaaabab7c709225703bb9ded0364cce89
IvanBukvic <32536551+IvanBukvic@users.noreply.github.com>
2021-03-31 20:59:59 +0200

    Add LimitedQueue class

---
+++ Model/LimitedQueue.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Vsite.Oom.Battleship.Model
+{
+    public class LimitedQueue<T> : Queue<T>
+    {
+
+        private readonly int Length;
+
+        public LimitedQueue(int length)
+        {
+            Length = length;
+        }
+
+        public new void Enqueue(T item)
+        {
+            base.Enqueue(item);
+            if (base.Count > Length)
+            {
+                Dequeue();
+            }
+        }
+
+
+    }
+}
---
+++ UnitTests/TestLimitedQueue.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace Vsite.Oom.Battleship.Model.UnitTests
+{
+    [TestClass]
+    public class TestLimitedQueue
+    {
+        [TestMethod]
+        public void LimitedQueueEnqueueMethodAddsElementsToQueue()
+        {
+
+            LimitedQueue<int> lq = new LimitedQueue<int>(3);
+
+            lq.Enqueue(1);
+            Assert.AreEqual(1, lq.Count);
+            lq.Enqueue(5);
+            Assert.AreEqual(2, lq.Count);
+            lq.Enqueue(8);
+            Assert.AreEqual(3, lq.Count);
+        }
+
+        [TestMethod]
+        public void LimitedQueueEnqueueMethodDequeuesElementsFromQueue()
+        {
+
+            LimitedQueue<int> lq = new LimitedQueue<int>(3);
+
+            lq.Enqueue(1);            
+            lq.Enqueue(5);            
+            lq.Enqueue(8);
+            lq.Enqueue(10);
+            Assert.AreEqual(3, lq.Count);
+            Assert.AreEqual(5, lq.Peek());
+        }
+
+        [TestMethod]
+        public void LimitedQueueEnqueueMethodReturnsCurrentElements()
+        {
+            LimitedQueue<int> lq = new LimitedQueue<int>(3);
+
+            lq.Enqueue(1);
+            lq.Enqueue(5);
+            lq.Enqueue(8);
+            lq.Enqueue(10);
+
+            var array = lq.ToArray();
+            Assert.AreEqual(5, array);
+            Assert.AreEqual(5, lq.Peek());
+        }
+    }
+}
