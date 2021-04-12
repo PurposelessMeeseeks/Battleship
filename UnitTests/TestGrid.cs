945bd45fc551654757a9f85f4949cf78e380e24b
Mateo Jadrušić <majadrus@vsite.hr>
2021-04-12 21:21:25 +0200

    Create Grid class

---
+++ UnitTests/TestGrid.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Linq;
+
+namespace Vsite.Oom.Battleship.Model.UnitTests
+{
+    [TestClass]
+    public class TestGrid
+    {
+        [TestMethod]
+        public void GetAvailablePlacementsReturns2SequencesForShip3SquaresLongOnGrid1x4()
+        {
+            Grid grid = new Grid(1, 4);
+            var placements = grid.GetAvailablePlacements(3);
+            Assert.AreEqual(2, placements.Count());
+        }
+
+        [TestMethod]
+        public void GetAvailablePlacementsReturns3SequencesForShip3SquaresLongOnGrid5x1()
+        {
+            Grid grid = new Grid(5, 1);
+            var placements = grid.GetAvailablePlacements(3);
+            Assert.AreEqual(3, placements.Count());
+        }
+    }
+}
