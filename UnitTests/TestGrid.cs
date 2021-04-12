1e9b3cf3d15d1277a229fc4699a32123fa8fc379
Julijan Sribar <julijan.sribar@gmail.com>
2021-04-12 20:17:21 +0200

    Add Grid class and unit tests

---
+++ UnitTests/TestGrid.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Linq;
+
+namespace Vsite.Oom.Battleship.Model.UnitTests
+{
+    [TestClass]
+    public class TestGrid
+    {
+        [TestMethod]
+        public void GetSequencesReturns2ArraysForAShipOfLength3SquaresOnGridWith1Row4Columns()
+        {
+            Grid grid = new Grid(1, 4);
+            var placements = grid.GetSequences(3);
+            Assert.AreEqual(2, placements.Count());
+            Assert.AreEqual(3, placements.ElementAt(0));
+            Assert.AreEqual(3, placements.ElementAt(1));
+        }
+
+        [TestMethod]
+        public void GetSequencesReturns3ArraysForAShipOfLength3SquaresOnGridWith5Rows1Column()
+        {
+            Grid grid = new Grid(5, 1);
+            var placements = grid.GetSequences(3);
+            Assert.AreEqual(3, placements.Count());
+            Assert.AreEqual(3, placements.ElementAt(0));
+            Assert.AreEqual(3, placements.ElementAt(1));
+            Assert.AreEqual(3, placements.ElementAt(2));
+        }
+
+    }
+}
