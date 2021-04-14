20566fe8822026ae0b07fb83e5b7b0ae46c0c44e
Danijel Krpes <dakrpes@vsite.hr>
2021-04-14 21:04:03 +0200

    Add Shipwright class

--- Model/Grid.cs
+++ Model/Grid.cs
@@ -53,6 +53,11 @@ namespace Vsite.Oom.Battleship.Model
             return result;
         }
 
+        internal void Eliminate(IEnumerable<Square> selected)
+        {
+            throw new NotImplementedException();
+        }
+
         private List<List<Square>> GetVerticalPlacements(int length)
         {
             var result = new List<List<Square>>();
