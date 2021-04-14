11fe3357ad1ba9dbcea6055bccb4bd1314a26335
Valentina Behin <72219767+ValentinaBehin@users.noreply.github.com>
2021-04-14 20:20:58 +0200

    Add fleet and ship

---
+++ Model/Fleet.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+
+namespace Vsite.Oom.Battleship.Model
+{
+    public class Fleet
+    {
+        public void CreateShip(IEnumerable<Square> squares)
+        {
+            Ships ship = new Ships(squares);
+            ships.Add(ship);
+        }
+
+        public IEnumerable<Ship> Ships
+        {
+            get { return ships; }
+        }
+
+        List<Ship> ships = new List<Ship>();
+    }
+}
