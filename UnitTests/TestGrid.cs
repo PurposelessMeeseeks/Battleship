46db48134901db0cd788df3d1d0a65c46eec75d1
Ivan Mihin <56129012+Ivan-Mihin@users.noreply.github.com>
2021-04-14 19:43:20 +0200

    Add test for length 1

--- UnitTests/TestGrid.cs
+++ UnitTests/TestGrid.cs
@@ -22,5 +22,13 @@ namespace Vsite.Oom.Battleship.Model.UnitTests
             var placements = grid.GetAvailablePlacements(3);
             Assert.AreEqual(3, placements.Count());
         }
+
+        [TestMethod]
+        public void GetAvailablePlacementsReturns3SequencesForShip1SquareLongOnGrid10x10()
+        {
+            Grid grid = new Grid(10, 10);
+            var placements = grid.GetAvailablePlacements(1);
+            Assert.AreEqual(100, placements.Count());
+        }
     }
 }
