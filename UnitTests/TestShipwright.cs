b9f8366ac2d627324013980c0bac39b482e6bd2a
Kresimir Bratkovic <kbratkovic@gmail.com>
2021-04-14 21:08:49 +0200

    Add Shipwright class and TestShipwright

---
+++ UnitTests/TestShipwright.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+
+
+
+namespace Vsite.Oom.Battleship.Model.UnitTests
+{
+    [TestClass]
+    public class TestShipwright
+    {
+        [TestMethod]
+        public void CreateFleetReturnsValidFleet()
+        {
+            List<int> shipLengths = new List<int> { 5, 4, 3, 2, 1 };
+            var shipwright = new Shipwright(10, 10, shipLengths);
+            var fleet = shipwright.CreateFleet();
+            Assert.AreEqual(5, fleet.Ships);
+        }
+    }
+}
