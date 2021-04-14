9debb77f05924791ec8247ef5b7dc913c82537cf
Danijel Krpes <dakrpes@vsite.hr>
2021-04-14 21:29:39 +0200

    Add SurroundingSquaresEliminator class

---
+++ Model/SurroundingSquaresEliminator.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Vsite.Oom.Battleship.Model
+{
+    public class SurroundingSquaresEliminator : ISquareEliminator
+    {
+        public SurroundingSquaresEliminator(int rows, int columns)
+        {
+            this.rows = rows;
+            this.columns = columns;
+        }
+        public IEnumerable<Square> ToEliminate(IEnumerable<Square> shipSquares)
+        {
+            throw new NotImplementedException();
+        }
+
+        private readonly int rows;
+        private readonly int columns;
+    }
+}
