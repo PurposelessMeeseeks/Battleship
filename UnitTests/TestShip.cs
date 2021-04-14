478f7cbb40ddb1be7bc164f955beda77dbf2d4ae
Danijel Krpes <dakrpes@vsite.hr>
2021-04-14 20:20:25 +0200

    Add fleet

--- UnitTests/TestShip.cs
+++ UnitTests/TestShip.cs
@@ -23,11 +23,9 @@ namespace Vsite.Oom.Battleship.Model.UnitTests
         {
             List<Square> squares = new List<Square> { new Square(1, 2), new Square(1, 3), new Square(1, 4) };
             var ship = new Ship(squares);
-            Assert.IsTrue(ship.Squares.Contains(new Square(5, 4)));
-            Assert.IsTrue(ship.Squares.Contains(new Square(1, 1)));
-            Assert.IsTrue(ship.Squares.Contains(new Square(1, 5)));
-
             Assert.IsFalse(ship.Squares.Contains(new Square(5, 4)));
+            Assert.IsFalse(ship.Squares.Contains(new Square(1, 1)));
+            Assert.IsFalse(ship.Squares.Contains(new Square(1, 5)));
         }
     }
 }
