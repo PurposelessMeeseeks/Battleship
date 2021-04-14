b9857261d5b78e773773684d2c0a8b242812dca3
Obradovic Igor <extern.igor.obradovic@porsche.de>
2021-04-14 20:02:48 +0200

    Add ship class

---
+++ Model/Ship.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Vsite.Oom.Battleship.Model
+{
+    public class Ship
+    {
+        public Ship(IEnumerable<Square> squares)
+        {
+            this.squares = squares.ToArray();
+        }
+
+        public IEnumerable<Square> Squares
+        {
+            get { return squares;  }
+        }
+
+        private Square[] squares;
+    }
+}
