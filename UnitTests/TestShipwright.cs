3cc3f7a62200b5aac447486fcec96c7dd4885e61
Ivan Čulina <72503859+ivculina@users.noreply.github.com>
2021-04-15 20:14:25 +0200

    Add square Eliminators interface and class

--- UnitTests/TestShipwright.cs
+++ UnitTests/TestShipwright.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace Vsite.Oom.Battleship.Model.UnitTests
@@ -13,7 +14,7 @@ namespace Vsite.Oom.Battleship.Model.UnitTests
             List<int> shipLengths = new List<int> { 5, 4, 3, 2, 1 };
             var shipwright = new Shipwright(10, 10, shipLengths);
             var fleet = shipwright.CreateFleet();
-            Assert.AreEqual(5, fleet.Ships);
+            Assert.AreEqual(5, fleet.Ships.Count());
         }
     }
 }
