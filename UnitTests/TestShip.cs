a8478a4ff6a7d13610b0d0659a8035a6c89052d9
Mateo Jadrušić <majadrus@vsite.hr>
2021-04-16 23:12:17 +0200

    Add Ship class

---
+++ UnitTests/TestShip.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Vsite.Oom.Battleship.Model.UnitTests
+{
+    [TestClass]
+    public class TestShip
+    {
+        [TestMethod]
+        public void ConstructorCreatesShipFromSquaredProvided()
+        {
+            List<Square> squares = new List<Square> { new Square(1, 2), new Square(1, 3), new Square(1, 4) };
+            var ship = new Ship(squares);
+            Assert.IsTrue(ship.Squares.Contains(squares[0]));
+            Assert.IsTrue(ship.Squares.Contains(squares[1]));
+            Assert.IsTrue(ship.Squares.Contains(squares[2]));
+
+            Assert.IsFalse(ship.Squared.Contains(new Square(5, 4)));
+        }
+
+        [TestMethod]
+        public void SquaresContainsOnlyGivenElementsInConstructor()
+        {
+            List<Square> squares = new List<Square> { new Square(1, 2), new Square(1, 3), new Square(1, 4) };
+            var ship = new Ship(squares);
+            
+            Assert.IsFalse(ship.Squared.Contains(new Square(5, 4)));
+            Assert.IsFalse(ship.Squared.Contains(new Square(1, 5)));
+            Assert.IsFalse(ship.Squared.Contains(new Square(1, 5)));
+        }
+    }
+}
