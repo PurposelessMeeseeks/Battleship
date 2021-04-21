459be2e4b07b3189142c47f0135b800f73666ecf
Mislav <mislav.miseric@gmail.com>
2021-04-21 23:06:20 +0200

    SurroundingSquareEliminator + testovi

---
+++ Model/SurroundingSquareEliminator .cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Vsite.Oom.Battleship.Model
+{
+    public class SurroundingSquareEliminator : ISquareEliminator
+    {
+        public SurroundingSquareEliminator(int rows, int columns)
+        {
+            this.rows = rows;
+            this.columns = columns;
+        }
+        public IEnumerable<Square> ToEliminate(IEnumerable<Square> shipSquares)
+        {
+            var result = new List<Square>();
+
+            int minCol = columns - 1;
+            int minRow = rows - 1;
+            int maxCol = 0;
+            int maxRow = 0;
+
+            shipSquares.ToList().ForEach(s =>
+            {
+                result.Add(s);
+                if(s.Row < minRow)
+                {
+                    minRow = s.Row;
+                }
+                if (s.Row > maxRow)
+                {
+                    maxRow = s.Row;
+                }
+                if (s.Column < minCol)
+                {
+                    minCol = s.Column;
+                }
+                if (s.Column > maxCol)
+                {
+                    maxCol = s.Column;
+                }
+            });
+
+            minRow = minRow > 0 ? minRow - 1 : 0;
+            minCol = minCol > 0 ? minCol - 1 : 0;
+            maxRow = maxRow < rows - 1 ? maxRow + 1 : maxRow;
+            maxCol = maxCol < columns - 1  ? maxCol + 1 : maxCol;
+
+            for (int i = minRow; i <= maxRow; i++)
+            {
+                for(int j = minCol; j <= maxCol; j++)
+                {
+                    Square sq = new Square(i,j);
+                    if (!result.Contains(sq))
+                        result.Add(sq);
+                }
+            }
+
+            return result;
+        }
+
+        private readonly int rows;
+        private readonly int columns;
+    }
+}
---
+++ UnitTests/TestSurroundingSquareEliminator.cs
@@ -0,0 +1,75 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Vsite.Oom.Battleship.Model.UnitTests
+{
+    [TestClass]
+    public class TestSurroundingSquareEliminator
+    {
+        [TestMethod]
+        public void ToEliminateReturns18SquaresForShip4_3To4_6()
+        {
+            var squares = new List<Square> { new Square(4, 3), new Square(4, 4), new Square(4, 5), new Square(4, 6) };
+            SurroundingSquareEliminator eliminator = new SurroundingSquareEliminator(10, 10);
+            var result = eliminator.ToEliminate(squares);
+
+            squares.Add(new Square(3, 2));
+            squares.Add(new Square(5, 2));
+            squares.Add(new Square(3, 7));
+            squares.Add(new Square(5, 7));
+
+            squares.ForEach(s => Assert.IsTrue(result.Contains(s)));
+            Assert.AreEqual(18, result.Count());
+        }
+
+        [TestMethod]
+        public void ToEliminateReturns8SquaresForShip3_9To4_9()
+        {
+            var squares = new List<Square> { new Square(3, 9), new Square(4, 9) };
+            SurroundingSquareEliminator eliminator = new SurroundingSquareEliminator(10, 10);
+            var result = eliminator.ToEliminate(squares);
+
+            squares.Add(new Square(2, 8));
+            squares.Add(new Square(2, 9));
+            squares.Add(new Square(5, 8));
+            squares.Add(new Square(5, 9));
+
+            squares.ForEach(s => Assert.IsTrue(result.Contains(s)));
+            Assert.AreEqual(8, result.Count());
+        }
+            
+        [TestMethod]
+        public void ToEliminateReturns12SquaresForShip7_5To9_5()
+        {
+            var squares = new List<Square> { new Square(7, 5), new Square(8, 5), new Square(9, 5) };
+            SurroundingSquareEliminator eliminator = new SurroundingSquareEliminator(10, 10);
+            var result = eliminator.ToEliminate(squares);
+
+            squares.Add(new Square(6, 4));
+            squares.Add(new Square(6, 6));
+            squares.Add(new Square(9, 4));
+            squares.Add(new Square(9, 6));
+
+            squares.ForEach(s => Assert.IsTrue(result.Contains(s)));
+            Assert.AreEqual(12, result.Count());
+        }
+
+        [TestMethod]
+        public void ToEliminateReturns9SquaresForShip0_5To1_5()
+        {
+            var squares = new List<Square> { new Square(0, 5), new Square(1, 5) };
+            SurroundingSquareEliminator eliminator = new SurroundingSquareEliminator(10, 10);
+            var result = eliminator.ToEliminate(squares);
+
+            squares.Add(new Square(0, 4));
+            squares.Add(new Square(0, 6));
+            squares.Add(new Square(2, 4));
+            squares.Add(new Square(2, 6));
+
+            squares.ForEach(s => Assert.IsTrue(result.Contains(s)));
+            Assert.AreEqual(9, result.Count());
+        }
+    }
+}
