1fcf16a83038f8131694c4bb737e1168a17d93b8
Obradovic Igor <extern.igor.obradovic@porsche.de>
2021-04-21 20:43:28 +0200

    Extend grid unit tests

--- UnitTests/TestGrid.cs
+++ UnitTests/TestGrid.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
 using System;
+using System.Collections.Generic;
 using System.Linq;
 
 namespace Vsite.Oom.Battleship.Model.UnitTests
@@ -28,10 +29,31 @@ namespace Vsite.Oom.Battleship.Model.UnitTests
         [TestMethod]
         public void GetAvailablePlacesReturnsOneHundredSequencesForShipOneSquareLongOnGreedTenByTen()
         {
+            //
             Grid grid = new Grid(10, 10);
             var placements = grid.GetAvailablePlacements(1);
 
             Assert.AreEqual(100, placements.Count());
+        }
+
+        [TestMethod]
+        public void GetAvailablePlacementsReturnsThreeSequencesForShipTwoSquareLongOnGridOneBySixWithSqaureZeroByTwoEliminated()
+        { 
+            Grid grid = new Grid(1, 6);
+            grid.Eliminate(new List<Square> { new Square(0, 2) });
+            var placements = grid.GetAvailablePlacements(2);
+
+            Assert.AreEqual(3, placements.Count());
+        }
+
+        [TestMethod]
+        public void GetAvailablePlacementsReturnsThreeSequencesForShipTwoSquareLongOnGridOneByFiveWithSqaureOneByZeroEliminated()
+        {
+            Grid grid = new Grid(5, 1);
+            grid.Eliminate(new List<Square> { new Square(1, 0) });
+            var placements = grid.GetAvailablePlacements(2);
+
+            Assert.AreEqual(2, placements.Count());
         }
     }
 }
