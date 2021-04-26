f6fc345626a84066e5706c8eec432f8f5c3ed1eb
fatigurqiti <fati.gurciti1@gmail.com>
2021-04-26 21:25:44 +0200

    Ship class created

---
+++ UnitTests/TestShip.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace Vsite.Oom.Battleship.Model.UnitTests
+{
+    [TestClass]
+    public class TestShip
+    {
+        [TestMethod]
+        public void ConstructorCreatesAShipFormSquaresProvided()
+        {
+            var sqaures = new List<Square> { new Square(4, 3), new Square(4, 4), new Square(4, 5), new Square(4, 6) };
+            Ship ship = new Ship(squares);
+            Assert.AreEqual(4, ship.Sqaures.Count());
+            Assert.IsTrue(ship.Sqaures.Contains(sqaures[0]));
+            Assert.IsTrue(ship.Sqaures.Contains(sqaures[1]));
+            Assert.IsTrue(ship.Sqaures.Contains(sqaures[2]));
+        }
+    }
+}
