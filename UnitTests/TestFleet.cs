08c5e75372c7bab1239ec46751be9ddd16ca3774
Julijan Sribar <julijan.sribar@gmail.com>
2021-04-26 21:34:33 +0200

    Create Fleet class

---
+++ UnitTests/TestFleet.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace Vsite.Oom.Battleship.Model.UnitTests
+{
+    [TestClass]
+    public class TestFleet
+    {
+        [TestMethod]
+        public void CreateShipAddsNewShipFromSquaresProvidedToTheFleet()
+        {
+            var squares = new List<Square> { new Square(4, 3), new Square(4, 4), new Square(4, 5), new Square(4, 6) };
+            Fleet fleet = new Fleet();
+            Assert.AreEqual(0, fleet.Ships.Count());
+            fleet.CreateShip(squares);
+            Assert.AreEqual(1, fleet.Ships.Count());
+
+            squares = new List<Square> { new Square(3, 8), new Square(4, 8) };
+            fleet.CreateShip(squares);
+            Assert.AreEqual(2, fleet.Ships.Count());
+        }
+    }
+}
