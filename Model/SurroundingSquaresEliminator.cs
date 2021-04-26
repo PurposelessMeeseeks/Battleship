299813dd7720f5a53cfa0709933743ec9bffd76a
Ante Žunić <anzunic@vsite.hr>
2021-04-26 12:15:44 +0200

    Implemented TestSorroundingSquareEliminator

--- Model/SurroundingSquaresEliminator.cs
+++ Model/SurroundingSquaresEliminator.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace Vsite.Oom.Battleship.Model
 {
-    class SurroundingSquaresEliminator : ISquareEliminator
+    public class SurroundingSquaresEliminator : ISquareEliminator
     {
         public SurroundingSquaresEliminator(int rows, int columns)
         {
@@ -15,8 +15,34 @@ namespace Vsite.Oom.Battleship.Model
         }
         public IEnumerable<Square> ToEliminate(IEnumerable<Square> shipSquares)
         {
-            //prode kroz shipsquares, uzme stupac prvog i zadnjeg polja da determinira jeli horizontalan ili vertikalan
-            throw new NotImplementedException();
+            
+            List<Square> shipSquaresList = shipSquares.ToList();
+            int topLeftX = shipSquaresList[0].Row - 1;
+            if (topLeftX < 0)
+                topLeftX = 0;
+            int topLeftXTemp = topLeftX;
+            int topLeftY = shipSquaresList[0].Column - 1;
+            if (topLeftY < 0)
+                topLeftY = 0;
+            int bottomRightX = shipSquaresList[shipSquaresList.Count - 1].Row + 1;
+            if (bottomRightX >= rows || bottomRightX >= columns)
+                bottomRightX = rows - 1;
+            int bottomRightY = shipSquaresList[shipSquaresList.Count - 1].Column + 1;
+            if (bottomRightY >= columns || bottomRightY >= columns)
+                bottomRightY = columns - 1;
+
+            List<Square> EliminatedSquares = new List<Square>();
+
+            for (; topLeftY < bottomRightY + 1; ++topLeftY)
+            {
+                for (topLeftX = topLeftXTemp; topLeftX < bottomRightX + 1; ++topLeftX)
+                {
+                    EliminatedSquares.Add(new Square(topLeftX, topLeftY));
+                }
+            }
+                
+
+            return EliminatedSquares;
         }
         private readonly int rows;
         private readonly int columns;
