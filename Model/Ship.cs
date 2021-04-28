2b79cf1e2ede4a1304965ed251ad52d0a04600f0
Kresimir Bratkovic <kbratkovic@gmail.com>
2021-04-28 21:09:57 +0200

    Implement SetSquareState method and Hit method

--- Model/Ship.cs
+++ Model/Ship.cs
@@ -6,6 +6,13 @@ using System.Threading.Tasks;
 
 namespace Vsite.Oom.Battleship.Model
 {
+    public enum HitResult
+    {
+        Missed,
+        Hit,
+        Sunken
+    }
+
     public class Ship
     {
         public Ship(IEnumerable<Square> squares)
@@ -16,6 +23,29 @@ namespace Vsite.Oom.Battleship.Model
         public IEnumerable<Square> Squares 
         {
             get { return squares; }
+        }
+
+        public HitResult Hit(Square square)
+        {
+            if (!squares.Contains(square))
+                return HitResult.Missed;
+
+            for (int i = 0; i < squares.Length; ++i)
+            {
+                if (squares[i].Equals(square))
+                    squares[i].SetSquareState(HitResult.Hit);
+            }
+
+            if (squares.All(sq => sq.SquareState == SquareState.Hit))
+            {
+                foreach (Square position in squares)
+                {
+                    position.SetSquareState(HitResult.Sunken);
+                }
+                return HitResult.Sunken;
+            }
+            
+            return HitResult.Hit;
         }
 
         private Square[] squares;
