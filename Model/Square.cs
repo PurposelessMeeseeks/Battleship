bb5a7b3aee20016711db9ec423dbd9d5424cee75
Ivan Posavac <ivposava@vsite.hr>
2021-04-28 21:00:51 +0200

    Missed, Hit, Sunken

--- Model/Square.cs
+++ Model/Square.cs
@@ -1,20 +1,72 @@
-﻿using System;
+﻿using System.Collections;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Vsite.Oom.Battleship.Model
 {
-    public struct Square
+    public enum SquareState
+    {
+        Default,
+        Missed,
+        Hit,
+        Sunken
+    }
+    public struct Square : IEnumerable<Square>
     {
         public Square(int row, int column)
         {
             Row = row;
             Column = column;
+            squareState = SquareState.Default;
         }
 
         public readonly int Row;
         public readonly int Column;
+
+        private SquareState squareState;
+
+        public SetSquareState(HitResult hitResult)
+        {
+            switch (hitResult)
+            {
+                case HitResult.Hit:
+                    break;
+                case HitResult.Sunken:
+                    break;
+                case HitResult.Missed:
+                    break;
+            }
+
+            
+        }
+        public SquareState SquareState
+        {
+            get { return squareState; }
+        }
+        public bool Equals(Square other)
+        {
+            return Row == other.Row && Column == other.Column;
+        }
+
+        public override bool Equals(object obj)
+        {
+            if (obj.GetType() != GetType())
+                return false;
+            return Equals((Square)obj);
+        }
+
+        public override int GetHashCode()
+        {
+            return Row ^ Column;
+        }
+
+        public IEnumerator<Square> GetEnumerator()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            throw new System.NotImplementedException();
+        }
     }
 }
--- UnitTests/TestSquare.cs
+++ UnitTests/TestSquare.cs
@@ -13,5 +13,14 @@ namespace Vsite.Oom.Battleship.Model.UnitTests
             Assert.AreEqual(5, s.Row);
             Assert.AreEqual(4, s.Column);
         }
+
+        [TestMethod]
+        public void SetSquareStateChangesSquareStateToMissedForHitResultMissed()
+        {
+            Square s = new Square(5, 4);
+            Assert.AreEqual(SquareState.Default, s.SquareState);
+            s.SetSquareState(HitResult.Sunken);
+            Assert.AreEqual(SquareState.Sunken, s.SquareState);
+        }
     }
 }
