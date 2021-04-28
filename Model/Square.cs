e3e67f2db5eb06f480a80a008684a2d3e3c1db83
Danijel Krpes <dakrpes@vsite.hr>
2021-04-28 21:01:07 +0200

    Implement HitResult and SquareStates

--- Model/Square.cs
+++ Model/Square.cs
@@ -6,15 +6,60 @@ using System.Threading.Tasks;
 
 namespace Vsite.Oom.Battleship.Model
 {
-    public struct Square
+    public enum SquareState
+    {
+        Default,
+        Missed,
+        Hit,
+        Sunken
+    }
+    public struct Square : IEquatable<Square>
     {
         public Square(int row, int column)
         {
             Row = row;
             Column = column;
+            squareState = SquareState.Default;
         }
 
         public readonly int Row;
         public readonly int Column;
+
+        private SquareState squareState;
+
+        public void SetSquareState(HitResult hitResult)
+        {
+            switch (hitResult)
+            {
+                case HitResult.Hit:
+                    break;
+                case HitResult.Missed:
+                    break;
+                case HitResult.Sunken:
+                    break;
+            }
+        }
+
+        public SquareState SquareState
+        {
+            get { return squareState; }
+        }
+
+        public bool Equals(Square other)
+        {
+            return Row == other.Row && Column == other.Column;
+        }
+
+        public override bool Equals(object obj)
+        {
+            if (obj.GetType() != GetType())
+                return false;
+            return base.Equals((Square)obj);
+        }
+
+        public override int GetHashCode()
+        {
+            return Row ^ Column;
+        }
     }
 }
