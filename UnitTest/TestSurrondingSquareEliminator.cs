faa114e9f6e722ae483111dc24704f90af73e038
Zdravko Pašalić <72557820+zdpasali@users.noreply.github.com>
2021-04-28 18:13:06 +0200

    resolve surrSquareElim (domaci)

--- UnitTest/TestSurrondingSquareEliminator.cs
+++ UnitTest/TestSurrondingSquareEliminator.cs
@@ -29,6 +29,49 @@ namespace Vsite.Oom.Battleship.Model.UnitTest
 
         }
 
+        [TestMethod]
+        public void ToEliminateReturns8SquaresForShip3_9To4_9()
+        {
+            var squares = new List<Square> { new Square(0, 3), new Square(0, 4) };
+            SurroundingSquareEliminator eliminator = new SurroundingSquareEliminator(10, 10);
+            var reuslt = eliminator.ToEliminate(squares);
+            Assert.Fail();
+
+        }
+
+        [TestMethod]
+        public void ToEliminateReturns8SquaresForShip3_4To4_9()
+        {
+            var squares = new List<Square> { new Square(3,9), new Square(4,9) };
+            SurroundingSquareEliminator eliminator = new SurroundingSquareEliminator(10, 10);
+            var reuslt = eliminator.ToEliminate(squares);
+            Assert.Fail();
+
+        }
+
+        [TestMethod]
+        public void ToEliminateReturns12SquaresForShip7_5To9_5()
+        {
+            var squares = new List<Square> { new Square(7,5), new Square(8,5), new Square(9, 5) };
+            SurroundingSquareEliminator eliminator = new SurroundingSquareEliminator(10, 10);
+            var reuslt = eliminator.ToEliminate(squares);
+            Assert.Fail();
+
+        }
+
+
+        [TestMethod]
+        public void ToEliminateReturns12SquaresForShip0_5To1_5()
+        {
+            var squares = new List<Square> { new Square(0,5), new Square(1,5) };
+            SurroundingSquareEliminator eliminator = new SurroundingSquareEliminator(10, 10);
+            var reuslt = eliminator.ToEliminate(squares);
+            Assert.Fail();
+
+        }
+
+
+
 
 
 
