bb902a183d5a6ac3ed55794d8906fbc068b9e41f
tirupcic <tirupcic@vsite.hr>
2021-04-29 22:28:10 +0200

    TestCommit

--- UnitTests/TestShip.cs
+++ UnitTests/TestShip.cs
@@ -1,7 +1,7 @@
 ﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
 using System;
-using System.Collections.Generic;
 using System.Linq;
+using System.Collections.Generic;
 
 namespace Vsite.Oom.Battleship.Model.UnitTests {
     [TestClass]
@@ -22,6 +22,26 @@ namespace Vsite.Oom.Battleship.Model.UnitTests {
             Assert.IsFalse(ship.Squares.Contains(new Square(5, 4)));
             Assert.IsFalse(ship.Squares.Contains(new Square(1, 1)));
             Assert.IsFalse(ship.Squares.Contains(new Square(1, 5)));
+        }
+        [TestMethod]
+        public void HitReturnsMissedIfSquareProvidedDoesntBelongToShip() {
+            List<Square> squares = new List<Square> { new Square(1, 2), new Square(1, 3), new Square(1, 4) };
+            var ship = new Ship(squares);
+            Assert.AreEqual(HitResult.Missed, ship.Hit(new Square(5, 4)));
+        }
+        [TestMethod]
+        public void HitReturnsMissedIfSquareProvidedBelongsToShip() {
+            List<Square> squares = new List<Square> { new Square(1, 2), new Square(1, 3), new Square(1, 4) };
+            var ship = new Ship(squares);
+            Assert.AreEqual(HitResult.Hit, ship.Hit(new Square(1, 2)));
+        }
+        [TestMethod]
+        public void HitReturnsMissedIfSquareProvidedIsLastSquareBelongingToShip() {
+            List<Square> squares = new List<Square> { new Square(1, 2), new Square(1, 3), new Square(1, 4) };
+            var ship = new Ship(squares);
+            Assert.AreEqual(HitResult.Hit, ship.Hit(new Square(1, 2)));
+            Assert.AreEqual(HitResult.Hit, ship.Hit(new Square(1, 4)));
+            Assert.AreEqual(HitResult.Sunken, ship.Hit(new Square(1, 3)));
         }
     }
 }
--- UnitTests/TestSquare.cs
+++ UnitTests/TestSquare.cs
@@ -5,10 +5,31 @@ namespace Vsite.Oom.Battleship.Model.UnitTests {
     [TestClass]
     public class TestSquare {
         [TestMethod]
-        public void SquareConstructorCreateASquareAtGivenRowAndColumn() {
+        public void SquareConstructorCreatesASquareAtGivenRowAndColumn() {
             Square s = new Square(5, 4);
-            Assert.AreEqual(5, s.row);
-            Assert.AreEqual(4, s.column);
+            Assert.AreEqual(5, s.Row);
+            Assert.AreEqual(4, s.Column);
+        }
+        [TestMethod]
+        public void SetSquareStateChangesSquareStateToMissedForHitResultMissed() {
+            Square s = new Square(5, 4);
+            Assert.AreEqual(SquareState.Default, s.SquareState);
+            s.SetSquareState(HitResult.Missed);
+            Assert.AreEqual(SquareState.Missed, s.SquareState);
+        }
+        [TestMethod]
+        public void SetSquareStateChangesSquareStateToHitForHitResultHit() {
+            Square s = new Square(5, 4);
+            Assert.AreEqual(SquareState.Default, s.SquareState);
+            s.SetSquareState(HitResult.Hit);
+            Assert.AreEqual(SquareState.Hit, s.SquareState);
+        }
+        [TestMethod]
+        public void SetSquareStateChangesSquareStateToSunkenForHitResultSunken() {
+            Square s = new Square(5, 4);
+            Assert.AreEqual(SquareState.Default, s.SquareState);
+            s.SetSquareState(HitResult.Sunken);
+            Assert.AreEqual(SquareState.Sunken, s.SquareState);
         }
     }
 }
