783c7df88c20d7c7cc0d1332f83d37a880b14fca
Jasmin Husić <jhsc1988@gmail.com>
2021-04-29 19:54:29 +0200

    Add methods and tests

--- Model/Square.cs
+++ Model/Square.cs
@@ -1,20 +1,64 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Vsite.Oom.Battleship.Model
 {
-    public struct Square
+    public enum SquareState
+    {
+        Default,
+        Missed,
+        Hit,
+        Sunken
+    }
+
+    public struct Square : IEquatable<Square>
     {
         public Square(int row, int column)
         {
             Row = row;
             Column = column;
+            squareState = SquareState.Default;
         }
 
         public readonly int Row;
         public readonly int Column;
+
+        private SquareState squareState;
+
+        public void SetSquareState(HitResult hitResult)
+        {
+            switch (hitResult)
+            {
+                case HitResult.Missed:
+                    // TODO:
+                    break;
+                case HitResult.Hit:
+                    // TODO:
+                    break;
+                case HitResult.Sunken:
+                    // TODO:
+                    break;
+            }
+            throw new NotImplementedException();
+        }
+        public SquareState SquareState
+        {
+            get { return squareState; }
+        }
+
+        public override int GetHashCode()
+        {
+            return Row ^ Column;
+        }
+        public bool Equals(Square other)
+        {
+            return Row == other.Row && Column == other.Column;
+        }
+        public override bool Equals(object obj)
+        {
+            if (obj.GetType() != GetType())
+                return false;
+            return Equals((Square)obj);
+        }
     }
+
 }
--- UnitTests/TestSquare.cs
+++ UnitTests/TestSquare.cs
@@ -11,7 +11,33 @@ namespace Vsite.Oom.Battleship.Model.UnitTests
             Square s = new Square(5, 4);
             Assert.AreEqual(5, s.Row);
             Assert.AreEqual(4, s.Column);
+        }
+        
+        [TestMethod]
+        public void SetSquareStateChangesSquareStateToMissedForHitResultMissed()
+        {
+            Square s = new Square(5, 4);
+            Assert.AreEqual(SquareState.Default,s.SquareState);
+            s.SetSquareState(HitResult.Missed);
+            Assert.AreEqual(SquareState.Missed, s.SquareState);
+        }        
 
+        [TestMethod]
+        public void SetSquareStateChangesSquareStateToHitForHitResultHit()
+        {
+            Square s = new Square(5, 4);
+            Assert.AreEqual(SquareState.Default,s.SquareState);
+            s.SetSquareState(HitResult.Hit);
+            Assert.AreEqual(SquareState.Hit, s.SquareState);
+        }        
+        
+        [TestMethod]
+        public void SetSquareStateChangesSquareStateToSunkenForHitResultSunken()
+        {
+            Square s = new Square(5, 4);
+            Assert.AreEqual(SquareState.Default,s.SquareState);
+            s.SetSquareState(HitResult.Sunken);
+            Assert.AreEqual(SquareState.Sunken, s.SquareState);
         }
     }
 }
