4cc1acd0d685689f33691ada1b90fe3bdd2292e1
Ante Žunić <anzunic@vsite.hr>
2021-05-05 19:49:48 +0200

    Add Hit method to  Fleet

--- Model/Fleet.cs
+++ Model/Fleet.cs
@@ -25,6 +25,19 @@ namespace Vsite.Oom.Battleship.Model
             get { return ships; }
         }
 
+        public HitResult Hit(Square square)
+        {
+            foreach(Ship ship in ships)
+            {
+                var hit = ship.Hit(square);
+                if (hit != HitResult.Missed)
+                {
+                    return hit;
+                }
+            }
+            return HitResult.Missed;
+        }
+
         private List<Ship> ships = new List<Ship>();
     }
 }
