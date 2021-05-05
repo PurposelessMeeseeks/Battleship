d0ca449847bce093a679e54b1790ed3d1e88b1df
IvanBukvic <32536551+IvanBukvic@users.noreply.github.com>
2021-05-05 21:09:29 +0200

    Add gunner class and shooting tactics

---
+++ Model/RandomShooting.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Vsite.Oom.Battleship.Model
+{
+    public class RandomShooting : ITargetSelect
+    {
+        private Grid Grid;
+        private int ShipLength;
+
+        public RandomShooting(Grid evidenceGrid, int shipLength)
+        {
+            Grid = evidenceGrid;
+            ShipLength = shipLength;
+        }
+
+        public Square NextTarget()
+        {
+
+            var allPlacements = Grid.GetAvailablePlacements(ShipLength);
+
+
+            throw new NotImplementedException();
+        }
+    }
+}
