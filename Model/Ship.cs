899f86563b0042cad533823a151247c85fea5d1d
Julijan Sribar <julijan.sribar@gmail.com>
2021-05-10 20:21:00 +0200

    Add square state

--- Model/Ship.cs
+++ Model/Ship.cs
@@ -6,6 +6,13 @@ using System.Threading.Tasks;
 
 namespace Vsite.Oom.Battleship.Model
 {
+    public enum HitResult
+    {
+        Missed,
+        Hit,
+        Sunken
+    }
+
     public class Ship
     {
         public Ship(IEnumerable<Square> squares)
@@ -16,6 +23,16 @@ namespace Vsite.Oom.Battleship.Model
         public IEnumerable<Square> Squares
         {
             get { return squares; }
+        }
+
+        public HitResult Hit(Square target)
+        {
+            // if target is not in squares collection then return Missed
+            // if target is one of squares then mark that square as Hit
+            // check if all squares are marked Hit:
+            //   if all squares are hit, then mark all squares sunken and return Sunken
+            //   else return Hit
+            throw new NotImplementedException();
         }
 
         private Square[] squares = null;
--- Model/Square.cs
+++ Model/Square.cs
@@ -6,15 +6,36 @@ using System.Threading.Tasks;
 
 namespace Vsite.Oom.Battleship.Model
 {
+    public enum SquareState
+    {
+        Default,
+        Missed,
+        Hit,
+        Sunken
+    }
+
     public struct Square
     {
         public Square(int row, int column)
         {
             Row = row;
             Column = column;
+            squareState = SquareState.Default;
+        }
+
+        public void SetState(SquareState newState)
+        {
+            squareState = newState;
+        }
+
+        public SquareState SquareState
+        {
+            get { return squareState; }
         }
 
         public readonly int Row;
         public readonly int Column;
+
+        private SquareState squareState;
     }
 }
