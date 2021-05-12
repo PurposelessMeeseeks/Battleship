b0ada914ae694774890a15b34b32c9a42300a6d3
Ivan Mihin <56129012+Ivan-Mihin@users.noreply.github.com>
2021-05-12 21:19:46 +0200

    Partially implement shooting tactics

--- Model/LinearShooting.cs
+++ Model/LinearShooting.cs
@@ -1,21 +1,61 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace Vsite.Oom.Battleship.Model
 {
+    public enum Orientation
+    {
+        Horizontal,
+        Vertical
+    }
+
     public class LinearShooting : ITargetSelect
     {
-        public LinearShooting(Grid grid, IEnumerable<int> shipLengths)
+        public LinearShooting(Grid grid, IEnumerable<Square> squaresHit)
         {
-
+            this.grid = grid;
+            this.squaresHit = new List<Square>(squaresHit.OrderBy(s => s.Row + s.Column));
         }
 
         public Square NextTarget()
         {
+            var orientation = GetHitSquaresOrientation();
+            List<IEnumerable<Square>> squares = new List<IEnumerable<Square>>();
+            switch (orientation)
+            {
+                case Orientation.Horizontal:
+                    squares.Add(grid.GetAvailablePlacementsInDirection(squaresHit[0], Direction.Leftwards));
+                    squares.Add(grid.GetAvailablePlacementsInDirection(squaresHit[1], Direction.Rightwards));
+                    break;
+                case Orientation.Vertical:
+                    squares.Add(grid.GetAvailablePlacementsInDirection(squaresHit[0], Direction.Upwards));
+                    squares.Add(grid.GetAvailablePlacementsInDirection(squaresHit[1], Direction.Downwards));
+                    break;
+                default:
+                    Debug.Assert(false);
+                    break;
+            }
+
             throw new NotImplementedException();
         }
+
+        Orientation GetHitSquaresOrientation()
+        {
+            if (squaresHit[0].Row == squaresHit[1].Row)
+            {
+                return Orientation.Horizontal;
+            }
+            else
+            {
+                return Orientation.Vertical;
+            }
+        }
+
+        private Grid grid;
+        private List<Square> squaresHit;
      }
 }
--- Model/SurroundingShooting.cs
+++ Model/SurroundingShooting.cs
@@ -8,14 +8,25 @@ namespace Vsite.Oom.Battleship.Model
 {
     public class SurroundingShooting : ITargetSelect
     {
-        public SurroundingShooting(Grid grid, IEnumerable<int> shipLengths)
+        public SurroundingShooting(Grid grid, Square firstHit)
         {
-
+            this.grid = grid;
+            this.firstHit = firstHit;
         }
 
         public Square NextTarget()
         {
+            List<IEnumerable<Square>> squares = new List<IEnumerable<Square>>();
+            squares.Add(grid.GetAvailablePlacementsInDirection(firstHit, Direction.Upwards));
+            squares.Add(grid.GetAvailablePlacementsInDirection(firstHit, Direction.Rightwards));
+            squares.Add(grid.GetAvailablePlacementsInDirection(firstHit, Direction.Downwards));
+            squares.Add(grid.GetAvailablePlacementsInDirection(firstHit, Direction.Leftwards));
+
+            // select one of them as target
             throw new NotImplementedException();
         }
+
+        private Grid grid;
+        private Square firstHit;
     }
 }
