71bd8371d1fe446597ff653de5f2fa99e9fbf193
Ivan Mihin <56129012+Ivan-Mihin@users.noreply.github.com>
2021-05-19 20:01:49 +0200

    Implement RandomShooting

--- Model/RandomShooting.cs
+++ Model/RandomShooting.cs
@@ -17,10 +17,33 @@ namespace Vsite.Oom.Battleship.Model
         public Square NextTarget()
         {
             var allPlacements = grid.GetAvailablePlacements(shipLength);
-            throw new NotImplementedException();
+
+            // create simple array of all squares
+            var allCandidates = allPlacements.SelectMany(seq => seq);
+
+            // create groups with individual values
+            var groups = allCandidates.GroupBy(sq => sq);
+
+            // find number of squares in the largest group
+            var maxCount = groups.Max(g => g.Count());
+
+            // filter groups with count == maxCount
+            var largestGroups = groups.Where(g => g.Count() == maxCount);
+
+            // fetch keys from largestGroups
+            var mostCommonSquares = largestGroups.Select(g => g.Key);
+
+            if (mostCommonSquares.Count() == 1)
+            {
+                mostCommonSquares.First();
+            }
+
+            int index = random.Next(mostCommonSquares.Count());
+            return mostCommonSquares.ElementAt(index);
         }
 
         private Grid grid;
         private int shipLength;
+        private Random random = new Random();
     }
 }
