5431738f31b80309abfa9657fae4830e9c88310a
fatigurqiti <fati.gurciti1@gmail.com>
2021-05-24 20:41:15 +0200

    Implemented LinearShooting tactics

--- Model/LinearShooting.cs
+++ Model/LinearShooting.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -28,30 +29,69 @@ namespace Vsite.Oom.Battleship.Model
         }
         public Square NextTarget()
         {
-            List<IEnumerable<Square>> result = new List<IEnumerable<Square>>();
+            List<IEnumerable<Square>> squares = new List<IEnumerable<Square>>();
+            var sorted = SortSquares(squaresHit);
+            var firstSquare = sorted.First();
+            var lastSquare = sorted.Last();
             switch (orientation)
             {
                 case Orientation.Horizontal:
-                    // get squares from left from the leftmost square in 'squaresHit'
-                    // get squares from right from the rightmost square in 'squaresHit'
-                
-                    // result = grid.GetSequence();
-                  
+
+                    var left = grid.GetSequence(firstSquare, Direction.Left);
+                    if (left.Count() > 0)
+                        squares.Add(left);
+                    var right = grid.GetSequence(lastSquare, Direction.Right);
+                    if (right.Count() > 0)
+                        squares.Add(right);
                     break;
                 case Orientation.Vertical:
+                    { 
+                    var up = grid.GetSequence(firstSquare, Direction.Up);
+                        if (up.Count() > 0)
+                            squares.Add(up);
+                        var down = grid.GetSequence(lastSquare, Direction.Down);
+                        if (down.Count() > 0)
+                            squares.Add(down);
+
+                    }
                     // get squares from up from the topmost square in 'squaresHit'
                     // get squares from below from the bottommost square in 'squaresHit'
                     break;
+
+                default:
+                    Debug.Assert(false);
+                    break;
             }
+            //sort list from longest to shortest
+
+            // sort list from longest to shortest
+            var ordered = squares.OrderByDescending(seq => seq.Count());
+            int maxLen = squares.First().Count();
+            if (maxLen > shipLength - squaresHit.Count())
+                maxLen = shipLength - squaresHit.Count();
+            // filter out all arrays for which length is equal or larger then shiplength - 1 
+            var longestCandidates = ordered.Where(seq => seq.Count() >= maxLen);
+            int index = random.Next(longestCandidates.Count());
+            return longestCandidates.ElementAt(index).First();
+
+
+
+
             //from list 'squares' select only those which are longer than shipLength - squaresHit 
             //from remaining randomly select one
             //from selected list take first square
             throw new NotImplementedException();
         }
 
+        private List<Square> SortSquares(List<Square> squares)
+        {
+          return squares.OrderBy(s => s.Column + s.Row).ToList();
+        }
+
         private Grid grid;
         private List<Square> squaresHit;
         private int shipLength;
         private Orientation orientation;
+        private Random random = new Random();
     }
 }
