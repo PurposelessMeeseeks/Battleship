f2839f57a3e4ea9354932ced1b9c2dd53fd03a95
Jasmin Husić <jhsc1988@gmail.com>
2021-05-27 15:07:58 +0200

    bugfixes

--- UnitTests/TestGrid.cs
+++ UnitTests/TestGrid.cs
@@ -14,8 +14,16 @@ namespace Vsite.Oom.Battleship.Model.UnitTests
             Grid grid = new Grid(1, 4);
             var placements = grid.GetAvailablePlacements(3);
             Assert.AreEqual(2, placements.Count());
-        }           
-        
+        }
+
+        [TestMethod]
+        public void GetAvailablePlacementsReturns3SequencesForShip3SquaresLongOnGrid5x1()
+        {
+            Grid grid = new Grid(5, 1);
+            var placements = grid.GetAvailablePlacements(3);
+            Assert.AreEqual(3, placements.Count());
+        }
+
         [TestMethod]
         public void GetAvailablePlacementsReturns100SequencesForShip1SquaresLongOnGrid10x10()
         {
