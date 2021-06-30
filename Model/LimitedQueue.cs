e07c5b8d500c87dea77ada7e600f5129040b4ea8
Obradovic Igor <extern.igor.obradovic@porsche.de>
2021-06-30 18:40:47 +0200

    Remove not needed using directives

--- Model/LimitedQueue.cs
+++ Model/LimitedQueue.cs
@@ -1,12 +1,7 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Collections.Generic;
 
 namespace Vsite.Oom.Battleship.Model
 {
-
     public class LimitedQueue<T> : Queue<T>
     {
         public LimitedQueue(int length)
